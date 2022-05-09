e077daa398bfc8741d7ab3ac13a58231d86f3762
slstempler <29709880+slstempler@users.noreply.github.com>
2022-05-09 17:07:33 -0400

    First stage of portfolio greyboxing including JS-implemented navbar

---
+++ nav_script.js
@@ -0,0 +1,11 @@
+document.querySelector('nav').innerHTML = ` 
+    <div> 
+        <h3><a href="./index.html">Name and/or Logo</a></h3>
+    </div>
+    <ul>
+        <li><a href="/web_dev.html">Web Design</a></li>
+        <li><a href="/design.html">Graphic Design</a></li>
+        <li><a href="/video_editing.html">Video Editing</a></li>
+        <li><a href="/contact.html">Contact</a></li>
+    </ul>
+`
