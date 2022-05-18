e9e596b65b8c3bbf38e2ed21485e50f7bbf8a796
slstempler <29709880+slstempler@users.noreply.github.com>
2022-05-18 18:05:50 -0400

    Restyled home page, implemented JS dark mode toggle

--- nav_script.js
+++ nav_script.js
@@ -1,11 +1,13 @@
 document.querySelector('nav').innerHTML = ` 
     <div> 
-        <h3><a href="./index.html">Name and/or Logo</a></h3>
+        <h3><a href="./index.html">Sean Stempler</a></h3>
     </div>
     <ul>
+        <li id="li_toggle"><a href="#" onclick="dark_toggle()" class="flex"><img src="/assets/darkmode-nav.png" id="toggle-button"></a></li>
         <li><a href="/web_dev.html">Web Design</a></li>
         <li><a href="/design.html">Graphic Design</a></li>
         <li><a href="/video_editing.html">Video Editing</a></li>
         <li><a href="/contact.html">Contact</a></li>
     </ul>
+    </div>
 `
