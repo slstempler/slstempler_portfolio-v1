80aa046a5d8c39d9e0bf339d9e11c50773e94efa
slstempler <29709880+slstempler@users.noreply.github.com>
2022-05-19 18:08:51 -0400

    Implemented dropdown; refined main page design; began mobile site design

--- nav_script.js
+++ nav_script.js
@@ -1,13 +1,19 @@
 document.querySelector('nav').innerHTML = ` 
-    <div> 
-        <h3><a href="./index.html">Sean Stempler</a></h3>
-    </div>
-    <ul>
-        <li id="li_toggle"><a href="#" onclick="dark_toggle()" class="flex"><img src="/assets/darkmode-nav.png" id="toggle-button"></a></li>
-        <li><a href="/web_dev.html">Web Design</a></li>
-        <li><a href="/design.html">Graphic Design</a></li>
-        <li><a href="/video_editing.html">Video Editing</a></li>
-        <li><a href="/contact.html">Contact</a></li>
-    </ul>
-    </div>
+<div> 
+    <h3><a href="./index.html">Sean Stempler</a></h3>
+</div>
+<ul>
+    <li class="dropdown">
+        <button class="drop-button">Disciplines
+            <i>▼</i>
+        </button>
+        <div class="drop-content">
+            <a href="/web_dev.html">Web Development</a>
+            <a href="/design.html">Graphic Design</a>
+            <a href="/video_editing.html">Video Editing</a>
+        </div>
+    </li>
+    <li id="li_toggle"><a href="#" onclick="dark_toggle()"><img src="/assets/darkmode-nav.png" id="toggle-button"></a></li>
+</ul>
+</div>
 `
