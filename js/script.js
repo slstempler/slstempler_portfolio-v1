9b38321ca5ba6dd417ec205b7fdff98bee8b1375
slstempler <29709880+slstempler@users.noreply.github.com>
2022-05-25 17:33:43 -0400

    Updated script to handle relative filepaths for index.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 //flag for dark mode state
 let darkstate = "on";
+const currentPage = window.location.pathname;
 
 function dark_toggle() {
     //let element = document.getElementById("header");
@@ -9,13 +10,21 @@ function dark_toggle() {
     let toggled = document.getElementById("toggle-button");
 
     //check state, apply effects as needed and swap flag
-    if (darkstate == "on") {
+    if (darkstate == "on" && (currentPage == "/slstempler_portfolio/" || currentPage == "/index.html")) {
         darkstate = "off";
-        toggled.src = "/assets/lightmode-nav.png";
+        toggled.src = "./assets/lightmode-nav.png";
+    }
+    else if (darkstate == "on"){
+        darkstate = "off";
+        toggled.src = "../assets/lightmode-nav.png";
+    }
+    else if (darkstate == "off" && (currentPage == "/slstempler_portfolio/" || currentPage == "/index.html")){
+        darkstate = "on";
+        toggled.src = "./assets/darkmode-nav.png";
     }
     else if (darkstate == "off"){
         darkstate = "on";
-        toggled.src = "/assets/darkmode-nav.png";
+        toggled.src = "../assets/darkmode-nav.png";
     }
     else {
         console.log("oh no there has been an issue with the JS");
